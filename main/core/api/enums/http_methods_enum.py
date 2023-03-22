25b78c565f5877184e133e97938eafdcace8e63c
lauMv <42174201+lauMv@users.noreply.github.com>
2023-03-22 11:10:24 -0400

    adding enums to the projects

---
+++ main/core/api/enums/http_methods_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class HttpMethods(Enum):
+    """
+    list of endpoints that pivotal uses
+    """
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    PATCH = "PATCH"
---
+++ main/pivotal/api/enums/pivotal_request_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class PivotalActions(Enum):
+    """
+    list of endpoints that pivotal uses
+    """
+    GET = "stories"
+    POST = "stories"
+    PUT = "stories/"
+    DELETE = "stories/"
+
