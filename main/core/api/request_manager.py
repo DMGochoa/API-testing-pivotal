5d27d20ecbd28f404507a3e4ffefe3f4f5996a31
Camiloarceg <camiloarceg@gmail.com>
2023-03-22 18:18:29 -0500

    delete enum file, change enviroment variable and format request manager

--- main/core/api/request_manager.py
+++ main/core/api/request_manager.py
@@ -4,21 +4,20 @@ from main.core.utils.logger import logging
 
 
 class RequestManager:
-    """ Request Manager Class implementation
-    """
+    """Request Manager Class implementation"""
 
     __instance = None
 
     def __init__(self, config_file=""):
-        """ Constructor For Request Manager
+        """Constructor For Request Manager
 
         Args:
             config_file (str, optional): json file with needed configuration. Defaults to ./configuration.json.
         """
         if config_file == "":
-            self.__config = Json_Reader().open_json('./configuration.json')
+            self.__config = Json_Reader().open_json("./configuration.json")
 
-        __environment = Json_Reader().open_json('./environment.json')
+        __environment = Json_Reader().open_json("./environment.json")
 
         env_selected = self.__config.get("environment", "development")
         self.__env_users = __environment.get(env_selected).get("users")
@@ -27,19 +26,19 @@ class RequestManager:
         self.key = self.__env_users.get("admin").get("key")
         self.token = self.__env_users.get("admin").get("token")
         self.response = None
-    
+
     @staticmethod
     def get_instance():
-        """ Singleton implementation to return instance
+        """Singleton implementation to return instance
 
-            Returns: instance -> RequestManager
+        Returns: instance -> RequestManager
         """
         if RequestManager.__instance is None:
             RequestManager.__instance = RequestManager()
         return RequestManager.__instance
 
     def make_request(self, http_method, endpoint, payload=None, **kwargs):
-        """ central method to make a request
+        """central method to make a request
 
         Args:
             http_method (str): HTTP method
@@ -53,5 +52,5 @@ class RequestManager:
             method=HttpMethods[http_method].value,
             url=f"{self.url}{endpoint}",
             headers=self.headers,
-                                         )
+        )
         return self.response
